17850d26719105e98e2befbf3ee186e2e3f79bcf
Chamuditha Tennekoon <chamudithabhagya@gmail.com>
2024-02-18 10:54:15 +0530

    backgroun color change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -10,11 +8,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+      <div className='main'>
       <Hero></Hero>
-        </div>
-    </>
+      </div>
   )
 }
 
